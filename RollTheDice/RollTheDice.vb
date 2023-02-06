2c9906a57849ee4eb1d9f058c5bfbee0b7bb9499
JoshuaMakuch <joshuamakuch9@gmail.com>
2023-02-06 08:47:07 -0700

    Add project files.

---
+++ RollTheDice/RollTheDice.vb
@@ -0,0 +1,18 @@
+'Joshua Makuch
+'RCET0265
+'Winter 2023
+'RollTheDice
+'
+
+Option Strict On
+Option Explicit On
+
+Imports System
+
+Module RollTheDice
+    Sub Main()
+
+
+
+    End Sub
+End Module
